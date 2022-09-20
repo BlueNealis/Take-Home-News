597a5ec1be1692bf9aa39728a66876236e17fe10
Blue Jessen <canealisblue@gmail.com>
2022-09-20 15:51:32 -0600

    Set up Route in app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Dashboard from './Components/Dashboard'
 import DetailView from './Components/DetailView'
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
-      <Dashboard />
-      <DetailView />
+    <Switch>
+      <Route exact path='/'>
+        <Dashboard />
+      </Route>
+      <Route path='/detail'
+        <DetailView />
+      </Route>
+    </Switch>
 
     </div>
   );
