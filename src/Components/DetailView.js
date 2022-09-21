7d3e3cd6e69bd4bc9914ed27b631deae87d1a8e9
Blue Jessen <canealisblue@gmail.com>
2022-09-21 00:22:25 -0600

    Add back to main button

--- src/Components/DetailView.js
+++ src/Components/DetailView.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import Route from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import './DetailView.css'
-
 function DetailView({articleInfo}) {
   console.log(articleInfo)
-  return(
+  return(<>
+    <Link to='/'>
+      <h1 className='redirect-main'>Back To Main</h1>
+    </Link>
     <div className='article-container'>
       <img src={articleInfo.multimedia[1].url} alt={articleInfo.multimedia[1].caption}/>
-      <h1 className='title'>{articleInfo.title}</h1>
+      <div className='article-grab'>
+        <h1 className='title'>{articleInfo.title}</h1>
+        <p>{articleInfo.abstract}</p>
+      </div>
     </div>
+    </>
   )
 }
 
