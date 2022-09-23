1e72733d8ad7769d8ed0c14611e142b63f29d3a1
Blue Jessen <canealisblue@gmail.com>
2022-09-22 20:34:08 -0600

    Add prompts for cypress test of Dashboard

--- cypress/e2e/Dashboard.cy.js
+++ cypress/e2e/Dashboard.cy.js
@@ -1,8 +1,25 @@
-describe('empty spec', () => {
+describe('Dashboard', () => {
   beforeEach('Go to Dashboard', () => {
-    cy.intercept(`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${process.env.REACT_APP_API_KEY}`, {
-      fixture: '/worldNYT.json'
-    })
     cy.visit('http://localhost:3000/')
+    cy.intercept(`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${process.env.REACT_APP_API_KEY}`, {
+      fixture: '../fixtures/worldNYT.json'
+    })
   })
+
+  it('Should show cards in results ', () => {
+
+  })
+
+  it('Should allow user to select a different option of news and have it populate', {
+
+  })
+
+  it('Should change to a detail view when the user clicks on an article link' {
+
+  })
+
+  it('Should show an error message if no cards populate for a topic', {
+
+  })
+
 })
