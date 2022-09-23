06792eecdad4ebab8a9f7631302675c6cbf0dd8f
Blue Jessen <canealisblue@gmail.com>
2022-09-23 09:39:06 -0700

    Add rest of read me

--- README.md
+++ README.md
@@ -1,15 +1,11 @@
 Top News Today
 ## Table of Contents
-- [Introduction](#introduction)
+
 - [Technologies](#technologies)
 - [Set Up](#set-up)
 - [Features](#features)
-- [Future Features](#future-features)
 - [Contributors](#contributors)
 - [Project Specifications](#project-specifications)
-
-### Introduction
-
 
 ### Technologies
 - JavaScript
@@ -20,32 +16,28 @@ Top News Today
 - CSS
 
 ### Set Up
-1. Clone this [repository](https://github.com/Baskanbetul/rancid-tomatillos).
+1. Clone this [repository](https://github.com/BlueNealis/take-home-news).
 2. `cd` into the directory.
 3. Run `npm i install`.
 4. Run `npm start`.
 
 ### Wireframe
 
+![Wireframe](Wireframe.png)
+
 ### Features
 - User can scroll and see headlines of top news from the NY Times
 - User can click on article to get more details
 - User can redirect to full article from detail page
+![Redirect](Redirect.gif)
 - User can go back to main page from detail page
+![Back](BackToMain.gif)
 - User can select different type of news topics
-
-### Future Features
-- A search bar to search for movies and have them live filter.
-- Categories of genre
-- Organization by cast
-- User login
-- User can save movies they've watched and input their own rating
-- User can put movies in a future queue
-- User can put movies in a favorite section
+![Selection](ChangeSelection.gif)
 
 ### Contributors
 
 - [Blue Nealis](https://github.com/BlueNealis/)[LinkedIn](https://www.linkedin.com/in/blue-nealis/)
 
 ### Project Specifications
-- Project specs are located [here](https://frontend.turing.edu/projects/module-3/rancid-tomatillos-v3.html).
+- Project specs are located [here](https://mod4.turing.edu/projects/take_home/take_home_fe).
--- cypress/e2e/Dashboard.cy.js
+++ cypress/e2e/Dashboard.cy.js
@@ -1,34 +1,35 @@
 describe('Dashboard', () => {
-  beforeEach('Go to Dashboard', () => {
-    cy.visit('http://localhost:3000/')
-    cy.intercept(`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${process.env.REACT_APP_API_KEY}`, {
+  beforeEach(() => {
+    cy.intercept(`https://api.nytimes.com/svc/topstories/v2/world.json?api-key=${process.env.REACT_APP_API_KEY}
+`, {
       fixture: '../fixtures/worldNYT.json'
     })
+    cy.visit('http://localhost:3000/')
   })
 
   it('Should show cards in results ', () => {
-    cy.get('card').should('have.length', 4)
-    cy.get('card').eq(0).within(() => {
+    cy.get('.card').should('have.length', 4)
+    cy.get('.card').eq(0).within(() => {
       cy.contains('Ukraine War Comes Home to Russians as Putin Imposes Draft')
       cy.get('img')
     })
-    cy.get('card').eq(1).within(()=>{
+    cy.get('.card').eq(1).within(()=>{
       cy.contains('What’s Driving the Protests in Iran?')
       cy.get('img')
     })
-    cy.get('card').eq(2).within(() => {
+    cy.get('.card').eq(2).within(() => {
       cy.contains('2 Michael Jackson Impersonators Look Identical. The Resemblance Ends There.')
       cy.get('img')
     })
-    cy.get('card').eq(3).within(() => {
+    cy.get('.card').eq(3).within(() => {
       cy.contains('Catholics Outnumber Protestants in Northern Ireland for the First Time')
       cy.get('img')
     })
 
   })
 
-  it('Should allow user to select a different option of news and have it populate', {
-    cy.get('select').select('us')
+  it('Should allow user to select a different option of news and have it populate', () => {
+      cy.get('select').select('us')
       cy.intercept(`https://api.nytimes.com/svc/topstories/v2/us.json?api-key=${process.env.REACT_APP_API_KEY}`, {
         fixture: '../fixtures/us.json'
       })
@@ -36,7 +37,7 @@ describe('Dashboard', () => {
       cy.get('card').eq(0).within(() => {
         cy.contains()
         cy.get('img')
-      }
+      })
       cy.get('card').eq(1).within(()=>{
         cy.contains()
         cy.get('img')
@@ -47,7 +48,7 @@ describe('Dashboard', () => {
       })
     })
 
-  it('Should change to a detail view when the user clicks on an article link' {
+  it('Should change to a detail view when the user clicks on an article link', () => {
     cy.get('card').eq(1).within(() => {
       cy.get('h1').click()
     })
@@ -57,7 +58,7 @@ describe('Dashboard', () => {
     cy.url().should('eq', '')
   })
 
-  it('Should show an error message if no cards populate for a topic', {
+  it('Should show an error message if no cards populate for a topic', () => {
     cy.get('card').eq(0).within(() => {
       cy.get('h1').click()
     })
